e37b243e8e99b08a998ec8471f8d5c14f3bf17df
donghyeon0915 <dongdong0915_@naver.com>
2023-01-04 03:22:49 +0900

    UPDATE : utils.js 업데이트

--- scripts/utils.js
+++ scripts/utils.js
@@ -32,6 +32,47 @@ function convertImageTagAbsoluteURL(doc = document) {
     });
 }
 
+/** 일반 특수문자를 전각문자로 변환하는 함수
+ * @param {string} text - 변환할 문자열
+ * @returns {string} - 전각문자로 변환된 문자열
+ */
+function convertSingleCharToDoubleChar(text) {
+    // singleChar to doubleChar mapping
+    const map = {
+        '!': '！',
+        '%': '％',
+        '&': '＆',
+        '(': '（',
+        ')': '）',
+        '*': '＊',
+        '+': '＋',
+        ',': '，',
+        '-': '－',
+        '.': '．',
+        '/': '／',
+        ':': '：',
+        ';': '；',
+        '<': '＜',
+        '=': '＝',
+        '>': '＞',
+        '?': '？',
+        '@': '＠',
+        '[': '［',
+        '\\': '＼',
+        ']': '］',
+        '^': '＾',
+        _: '＿',
+        '`': '｀',
+        '{': '｛',
+        '|': '｜',
+        '}': '｝',
+        '~': '～',
+        ' ': ' ', // 공백만 전각문자가 아닌 FOUR-PER-EM SPACE로 변환
+    };
+    return text.replace(/[!%&()*+,\-./:;<=>?@\[\\\]^_`{|}~ ]/g, function (m) {
+        return map[m];
+    });
+}
 
 /**
 * 해당 값이 null 또는 undefined인지 체크합니다.
@@ -40,4 +81,33 @@ function convertImageTagAbsoluteURL(doc = document) {
 */
 function isNull(value) {
     return value === null || value === undefined;
+}
+
+/**
+ * base64로 문자열을 base64로 인코딩하여 반환합니다.
+ * @param {string} str - base64로 인코딩할 문자열
+ * @returns {string} - base64로 인코딩된 문자열
+ */
+function b64EncodeUnicode(str) {
+    return btoa(
+        encodeURIComponent(str).replace(/%([0-9A-F]{2})/g, function (match, p1) {
+            return String.fromCharCode(`0x${p1}`);
+        }),
+    );
+}
+
+/**
+ * base64로 인코딩된 문자열을 base64로 디코딩하여 반환합니다.
+ * @param {string} b64str - base64로 인코딩된 문자열
+ * @returns {string} - base64로 디코딩된 문자열
+ */
+function b64DecodeUnicode(b64str) {
+    return decodeURIComponent(
+        atob(b64str)
+            .split('')
+            .map(function (c) {
+                return `%${`00${c.charCodeAt(0).toString(16)}`.slice(-2)}`;
+            })
+            .join(''),
+    );
 }
