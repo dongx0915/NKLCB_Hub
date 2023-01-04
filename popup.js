692e9daeadd32e8d8642b9cf9f3614c0bfd33246
donghyeon0915 <donghyeon0915@gmail.com>
2023-01-04 22:09:50 +0900

    ADD : popup.html에 드롭다운 추가

--- popup.js
+++ popup.js
@@ -118,3 +118,19 @@ $('#onffbox').on('click', () => {
 //         });
 //     });
 // });
+
+const optionMenu = document.querySelector(".select-menu"),
+selectBtn = optionMenu.querySelector(".select-btn"),
+options = optionMenu.querySelectorAll(".option"),
+sBtn_text = optionMenu.querySelector(".sBtn-text");
+
+selectBtn.addEventListener("click", () => optionMenu.classList.toggle("active"));       
+
+options.forEach(option =>{
+option.addEventListener("click", ()=>{
+ let selectedOption = option.querySelector(".option-text").innerText;
+ sBtn_text.innerText = selectedOption;
+
+ optionMenu.classList.remove("active");
+});
+});
